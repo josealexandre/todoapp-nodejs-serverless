2dac3bf3cffa7ed79c6a98e252813b278cdbb676
Jose Alexandre <jose.alexandre@stoneage.com.br>
2021-06-17 20:19:11 -0300

    Create a ToDo and save in DynamoDb

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,11 +1,27 @@
-export const handle = async (event) => ({
-  statusCode: 201,
-  body: JSON.stringify({
-    message: 'Hello world!',
-  }),
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { v4 as uuidV4 } from 'uuid';
+import { dynamoDb } from '../utils/dynamoDbClient';
 
-// export default handle;
+export const handle: APIGatewayProxyHandler = async (event) => {
+  const { title, deadline } = JSON.parse(event.body);
+  const { userId } = event.pathParameters;
+
+  const newTodo = {
+    id: uuidV4(),
+    userId,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  await dynamoDb.put({
+    TableName: 'todos',
+    Item: newTodo,
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: '',
+  };
+};
